4b77e01ac62a57eb2a13b9e6987ecd638c081648
MrP123 <matthias.panny@mci.edu>
2025-04-17 17:47:08 +0900

    Changed plotting of errors to include outliers

--- nn_train.py
+++ nn_train.py
@@ -1,7 +1,14 @@
 import torch
 from torch import Tensor
+from torch.utils.data import DataLoader
 
-from nn_helper import get_dataset
+import matplotlib.pyplot as plt
+
+import numpy as np
+import numpy.typing as npt
+from typing import Callable
+
+from nn_helper import LocationsContainer, get_dataset
 from nn_models import NNYuEtAl
 
 def train_epoch(model: torch.nn.Module, dataset: torch.utils.data.Dataset, loss_fn: torch.nn.modules.loss._Loss, optimizer: torch.optim.Optimizer,
@@ -92,3 +99,110 @@ def train_model(model: torch.nn.Module, dataset: torch.utils.data.Dataset, loss_
         met["val_loss"].append(epoch_met["val_loss"])
 
     return met
+
+def test_model(model: torch.nn.Module, dataset: torch.utils.data.Dataset, locations: LocationsContainer,
+               error_fun: Callable[[npt.ArrayLike, npt.ArrayLike], float], device: int | str = "cpu") -> tuple[plt.Figure, plt.Axes, dict]:
+    model.eval()
+    model = model.to(device)
+
+    fig, ax = plt.subplots()
+    errors = {label: [] for label in locations.get_labels()}
+
+    with torch.no_grad():
+        for x, y in DataLoader(dataset, shuffle=True):
+            x, y = x.to(device, dtype=torch.float32), y.to(device, dtype=torch.float32)
+            y_np = y.reshape((2,)).numpy()
+
+            if isinstance(model, NNYuEtAl):
+                x = x.reshape(x.shape[0], 1, x.shape[1])
+
+            y_predict: Tensor = model(x)
+            y_pred_np = y_predict.detach().reshape((2,)).numpy()
+            location = locations.from_xy(y_np[0], y_np[1])
+
+            ax.plot(y_np[0], y_np[1], "o", mfc="none", mew=2, markersize=8, color=location.color, label="target - " + location.label)
+            ax.plot(y_pred_np[0], y_pred_np[1], "x", markersize=8, color=location.color, label="prediction - " + location.label, alpha=0.5)
+
+            error = error_fun(y_pred_np, y_np)
+            errors[location.label].append(error)
+
+            #ax.yaxis.set_inverted(True)
+            ax.set_xlim((-50, 50))
+            ax.set_ylim((70, -70)) #inverted!
+
+            ax.set_xlabel("X coordinate (mm)")
+            ax.set_ylabel("Y coordinate (mm)")
+
+            # force unique labels
+            handles, labels = ax.get_legend_handles_labels()
+            by_label = dict(zip(labels, handles))
+            ax.legend(by_label.values(), by_label.keys())
+
+    return fig, ax, errors
+
+def plot_violins_from_error(errors: dict[str, list[float]], threshold: float = 100) -> tuple[plt.Figure, plt.Axes]:
+    #ToDo clean up this mess! Make sure that only 2 axes are used when actual values were removed
+
+    errors = dict(sorted(errors.items())) # sort so that keys are in alphabetical order
+
+    fig, (ax_top, ax_bot) = plt.subplots(2, 1, sharex=True)
+    ax_bot: plt.Axes
+    ax_top: plt.Axes
+
+    fig.subplots_adjust(hspace=0.05)  # adjust space between Axes
+
+    all_data = [v for v in errors.values()]
+
+    removed_count = sum(1 for cat in all_data for val in cat if val > threshold) #check how many values will be removed
+    if removed_count > 0:
+        print(f"Removed {removed_count} values exceeding the threshold of {threshold}")
+        removed_data = [[val for val in cat if val > threshold] for cat in all_data]
+        all_data = [[val for val in cat if val <= threshold] for cat in all_data] #hack for quick thresholding
+
+    ax_bot.violinplot(all_data)
+    _, ax_bot_y_top = ax_bot.get_ylim()
+
+    # Find plotting range for outliers based on their min/max values
+    # Makes sure only points above the plotting range of the violin plots are considered
+    # ToDo: Refactor and check if working in all cases
+    #max_outlier = -np.inf
+    #min_outlier = +np.inf
+
+    if removed_count > 0:
+        for i, vals in enumerate(removed_data):
+            if len(vals) == 0:
+                continue
+            ax_top.plot(np.ones(len(vals)) * (i+1), vals, 'x')
+
+        #if np.max(vals) > max_outlier:
+        #    max_outlier = np.max(vals)
+
+        #if np.min(vals) < min_outlier:
+        #    min_outlier = np.min(vals)
+
+    #ax_top.set_ylim(bottom=min_outlier, top=max_outlier)  # outliers only
+
+    ax_bot.spines.top.set_visible(False)
+    ax_top.spines.bottom.set_visible(False)
+
+    ax_top.xaxis.tick_top()
+    ax_top.tick_params(labeltop=False)  # don't put tick labels at the top
+    ax_bot.xaxis.tick_bottom()
+
+    ax_bot.set_xticks([i + 1 for i in range(len(all_data))], labels=errors.keys())
+    ax_bot.set_ylabel("Error (mm)")
+
+    # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/broken_axis.html
+    d = .5  # proportion of vertical to horizontal extent of the slanted line
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+    ax_top.plot([0, 1], [0, 0], transform=ax_top.transAxes, **kwargs)
+    ax_bot.plot([0, 1], [1, 1], transform=ax_bot.transAxes, **kwargs)
+    
+    return fig, ax_bot
+
+
+if __name__ == "__main__":
+    errors = {'front': [np.float32(1.3877047), np.float32(0.4650986), np.float32(0.5736587), np.float32(0.6306847), np.float32(0.72050554), np.float32(0.49555248), np.float32(0.42456514), np.float32(2.5520551), np.float32(0.69600177), np.float32(0.50885177), np.float32(0.38532776), np.float32(0.9196282), np.float32(0.37682468), np.float32(0.91961294), np.float32(1.7318237), np.float32(0.8981945), np.float32(0.9165314), np.float32(2.5529287), np.float32(0.6140457), np.float32(0.7211996), np.float32(1.6954455), np.float32(0.39814422), np.float32(0.63076097), np.float32(1.3603944), np.float32(0.57313263), np.float32(0.57383794), np.float32(0.4244051), np.float32(0.9190333), np.float32(1.6927868), np.float32(2.5511892)], 'center': [np.float32(0.23155807), np.float32(0.24845217), np.float32(0.48596364), np.float32(0.20586996), np.float32(0.23126088), np.float32(0.19678019), np.float32(0.21014419), np.float32(0.20953189), np.float32(0.1778304), np.float32(0.21403803), np.float32(0.20629588), np.float32(0.20531254), np.float32(0.19573452), np.float32(0.18281722), np.float32(40.86105), np.float32(0.20643395), np.float32(0.20651706), np.float32(0.22505696), np.float32(0.48609045), np.float32(0.2191482), np.float32(0.20545661), np.float32(0.20125332), np.float32(0.48613265), np.float32(0.17709832), np.float32(0.21758877), np.float32(0.23174119), np.float32(40.86105), np.float32(0.20206489), np.float32(0.19248514), np.float32(0.23102918)], 'back': [np.float32(0.63613987), np.float32(0.5825399), np.float32(0.5244399), np.float32(0.78231835), np.float32(0.67898387), np.float32(0.63545346), np.float32(0.78707504), np.float32(0.91539264), np.float32(0.8008457), np.float32(0.51913035), np.float32(0.9988633), np.float32(0.52868134), np.float32(0.76627827), np.float32(0.17164242), np.float32(0.9130925), np.float32(0.6248225), np.float32(0.87640774), np.float32(0.7689523), np.float32(0.8530169), np.float32(0.6279541), np.float32(0.6252306), np.float32(0.48309714), np.float32(0.8187625), np.float32(0.5082445), np.float32(0.1708688), np.float32(0.9740913), np.float32(0.17391764), np.float32(0.5754909), np.float32(0.88909495), np.float32(0.6270349)]}
+    fig, ax = plot_violins_from_error(errors, threshold=100)
+    plt.show()
